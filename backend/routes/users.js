196d06adcbaa32209ae9517cb939fbbb20f95a65
Smauga <nsmagn@yandex.ru>
2022-05-16 13:12:47 +0700

    fix: исправить валидацию id пользователей и карточек

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,7 @@ router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().min(24).max(24),
+    userId: Joi.string().length(24).hex().required(),
   }),
 }), getUser);
 router.patch('/me', celebrate({
